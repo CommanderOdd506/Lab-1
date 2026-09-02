3d4abe1489b6f68683c48a259ca4546bb768dc69
CommanderOdd506 <155345740+CommanderOdd506@users.noreply.github.com>
2026-09-02 19:15:38 -0400

    bishop

--- My project/Assets/Piece.cs
+++ My project/Assets/Piece.cs
@@ -23,17 +23,32 @@ public class Piece : MonoBehaviour
             case PieceType.Pawn:
                 for(int i = -1; i < 2; i++)
                 {
-                    Vector3 center = new Vector3(i +0.5f, 1.5f, 0);
+                    Vector3 center = new Vector3(i +0.5f + position.x, 1.5f+ position.y, 0);
                     Vector3 size = new Vector3(1, 1, 0);
                     Gizmos.DrawCube(center, size);
                 }
                 break;
             case PieceType.Bishop:
-                for(int i = 0; i < 7; i++)
+                for(int i = -8; i < 7; i++)
                 {
-                    for(int j = 0; j < 7; j++)
+                    for(int j = -8; j < 7; j++)
                     {
+                        Vector3 center1 = new Vector3(i + 1.5f + position.x, j + 1.5f + position.y, 0);
+                        Vector3 size = new Vector3(1, 1, 0);
 
+                        if (i == j && (i != -1 && j!= -1))
+                        { Gizmos.DrawCube(center1, size); }
+                    }
+                }
+                for (int i = 7; i > -8; i--)
+                {
+                    for (int j = -7; j < 8; j++)
+                    {
+                        Vector3 center1 = new Vector3(i + .5f + position.x, j + .5f + position.y, 0);
+                        Vector3 size = new Vector3(1, 1, 0);
+                        
+                        if (-i == j && (i != 0 && j != 0))  
+                        { Gizmos.DrawCube(center1, size); }
                     }
                 }
                 break;
