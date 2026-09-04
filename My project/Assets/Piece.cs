3b4215519e56f9917478c1c69f79cfb73b7a7d98
CommanderOdd506 <155345740+CommanderOdd506@users.noreply.github.com>
2026-09-04 12:37:12 -0400

    Everything but the Knight

--- My project/Assets/Piece.cs
+++ My project/Assets/Piece.cs
@@ -33,32 +33,96 @@ public class Piece : MonoBehaviour
                 {
                     for(int j = -8; j < 7; j++)
                     {
-                        Vector3 center1 = new Vector3(i + 1.5f + position.x, j + 1.5f + position.y, 0);
+                        Vector3 center = new Vector3(i + 1.5f + position.x, j + 1.5f + position.y, 0);
                         Vector3 size = new Vector3(1, 1, 0);
 
                         if (i == j && (i != -1 && j!= -1))
-                        { Gizmos.DrawCube(center1, size); }
+                        { Gizmos.DrawCube(center, size); }
                     }
                 }
                 for (int i = 7; i > -8; i--)
                 {
                     for (int j = -7; j < 8; j++)
                     {
-                        Vector3 center1 = new Vector3(i + .5f + position.x, j + .5f + position.y, 0);
+                        Vector3 center = new Vector3(i + .5f + position.x, j + .5f + position.y, 0);
                         Vector3 size = new Vector3(1, 1, 0);
                         
                         if (-i == j && (i != 0 && j != 0))  
-                        { Gizmos.DrawCube(center1, size); }
+                        { Gizmos.DrawCube(center, size); }
                     }
                 }
                 break;
             case PieceType.Knight:
                 break;
             case PieceType.Rook:
+                for (int i = -8; i < 8; i++)
+                {
+                    Vector3 center = new Vector3(i + .5f + position.x, 0.5f + position.y, 0);
+                    Vector3 size = new Vector3(1, 1, 0);
+                    if (i != 0)
+                    { Gizmos.DrawCube(center, size); }
+                }
+                for (int i = -7; i < 8; i++)
+                {
+                    Vector3 center = new Vector3(.5f + position.x, i + 0.5f + position.y, 0);
+                    Vector3 size = new Vector3(1, 1, 0);
+                    if (i != 0)
+                    { Gizmos.DrawCube(center, size); }
+                }
                 break;
             case PieceType.Queen:
+                for (int i = -8; i < 7; i++)
+                {
+                    for (int j = -8; j < 7; j++)
+                    {
+                        Vector3 center = new Vector3(i + 1.5f + position.x, j + 1.5f + position.y, 0);
+                        Vector3 size = new Vector3(1, 1, 0);
+
+                        if (i == j && (i != -1 && j != -1))
+                        { Gizmos.DrawCube(center, size); }
+                    }
+                }
+                for (int i = 7; i > -8; i--)
+                {
+                    for (int j = -7; j < 8; j++)
+                    {
+                        Vector3 center = new Vector3(i + .5f + position.x, j + .5f + position.y, 0);
+                        Vector3 size = new Vector3(1, 1, 0);
+
+                        if (-i == j && (i != 0 && j != 0))
+                        { Gizmos.DrawCube(center, size); }
+                    }
+                }
+                for (int i = -8; i < 8; i++)
+                {
+                    Vector3 center = new Vector3(i + .5f + position.x, 0.5f + position.y, 0);
+                    Vector3 size = new Vector3(1, 1, 0);
+                    if (i != 0)
+                    { Gizmos.DrawCube(center, size); }
+                }
+                for (int i = -7; i < 8; i++)
+                {
+                    Vector3 center = new Vector3(.5f + position.x, i + 0.5f + position.y, 0);
+                    Vector3 size = new Vector3(1, 1, 0);
+                    if (i != 0)
+                    { Gizmos.DrawCube(center, size); }
+                }
                 break;
             case PieceType.King:
+                for (int i = -1; i < 2; i++)
+                {
+                    for (int j = -1; j < 2; j++)
+                    {
+                        Vector3 center = new Vector3(i + .5f + position.x,j+  0.5f + position.y, 0);
+                        Vector3 size = new Vector3(1, 1, 0);
+
+                        if (i == 0 && j == 0)
+                        {
+                            continue;
+                        }
+                        Gizmos.DrawCube(center, size);
+                    }
+                }
                 break;
             default:
                 break;
